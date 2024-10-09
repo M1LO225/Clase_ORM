bbfeb8fe9035ae5c7c3a27e6ab08f3d679840386
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2024-10-09 07:43:15 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_BD/Models/EstudianteUdla.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_BD.Models
+{
+    public class EstudianteUdla
+    {
+        [Key]
+        public int IdBanner { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        [EmailAddress]
+        public string Correo { get; set; }
+        public bool TieneBeca { get; set; }
+        public Carrera Carrera { get; set; }
+        [ForeignKey("Carrera")]
+        public int IdCarrera { get; set; }
+
+    }
+}
